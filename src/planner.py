8bf09811274499b83059cef37c6dae1e296b663b
Ellis Ratner <ellis.ratner@gmail.com>
2018-05-29 21:47:17 -0700

    Added signed dist funcs and duration constraint to the traj optim

--- src/planner.py
+++ src/planner.py
@@ -10,22 +10,42 @@ from pydrake.all import (
 )
 
 class Planner:
-    def __init__(self, plant, context, running_cost, signed_dist_func):
+    def __init__(self, plant, context, running_cost, signed_dist_funcs=[]):
         self.plant = plant
         self.context = context
         self.running_cost = running_cost
-        self.signed_dist_func = signed_dist_func
+        self.signed_dist_funcs = signed_dist_funcs
         self.opt_params = {'num_time_samples': 21,
                            'minimum_timestep': 0.1,
                            'maximum_timestep': 0.4}
 
-    def _solve_traj_opt(self, initial_state, final_state=None):
+    def _solve_traj_opt(self, initial_state, final_state=None, duration_bounds=None, verbose=False):
+        '''Finds a trajectory from an initial state, optionally to a final state.
+        
+        Args: 
+            initial_state (tuple): the initial state
+            final_state (tuple): the final state (default to None, final state unconstrained)
+            duration (tuple): the min and max duration of the trajectory (default to None, 
+                              no duration constraints)
+            verbose (bool):
+
+        Returns:
+            pydrake.trajectories.PiecewisePolynomial: the planned trajectory
+            float: the cost of the planned trajectory
+
+        Raises:
+            RuntimeError: raised if the optimization fails
+        '''
         traj_opt = DirectCollocation(self.plant, self.context, 
                                      self.opt_params['num_time_samples'],
                                      self.opt_params['minimum_timestep'],
                                      self.opt_params['maximum_timestep'])
 
         traj_opt.AddEqualTimeIntervalsConstraints()
+
+        # Add bounds on the total duration of the trajectory
+        if duration_bounds:
+            traj_opt.AddDurationBounds(duration_bounds[0], duration_bounds[1])
 
         # TODO make input limits a paramter
         limits_low = [-15., -15.]
@@ -37,7 +57,8 @@ class Planner:
             traj_opt.AddConstraintToAllKnotPoints(limits_low[i] <= u[i])
             traj_opt.AddConstraintToAllKnotPoints(u[i] <= limits_upp[i])
 
-        traj_opt.AddConstraintToAllKnotPoints(self.signed_dist_func(x) >= 0)
+        for signed_dist_func in self.signed_dist_funcs:
+            traj_opt.AddConstraintToAllKnotPoints(signed_dist_func(x) >= 0)
 
         traj_opt.AddRunningCost(self.running_cost(x, u))
 
@@ -55,16 +76,18 @@ class Planner:
             traj_opt.SetInitialTrajectory(PiecewisePolynomial(), initial_x_trajectory)
 
         result = traj_opt.Solve()
-        assert(result == SolutionResult.kSolutionFound)
+
+        if result != SolutionResult.kSolutionFound:
+            raise RuntimeError('Direct collocation failed from initial state {}!'.format(initial_state))
 
         state_samples = traj_opt.GetStateSamples()
         input_samples = traj_opt.GetInputSamples()
         total_cost = 0.
         for k in range(state_samples.shape[1]):
             total_cost += self.running_cost(state_samples[:, k], input_samples[:, k])
-            print(self.signed_dist_func(state_samples[:, k]))
-
-        print("Total cost: {}".format(total_cost))
+            if verbose:
+                for i, phi in enumerate(self.signed_dist_funcs):
+                    print("\tsigned dist {}: {}".format(i, signed_dist_func(state_samples[:, k])))
 
         return traj_opt.ReconstructStateTrajectory(), total_cost
 
--- src/run.py
+++ src/run.py
@@ -14,11 +14,37 @@ from pydrake.multibody.rigid_body_plant import (
 from underactuated import (
     PlanarRigidBodyVisualizer
 )
-
 from planner import Planner
 
 
-def main():
+def init_signed_dist_funcs():
+    import pydrake.symbolic as sym
+    signed_dist_funcs = []
+
+    wall_x = 2.
+    pole_length = 0.5
+    cart_width = 0.6
+    cart_height = 0.4
+
+    # Distance from the tip of the pole to the wall
+    def dist_pole_wall(x):
+        pole_x = pole_length * sym.cos(x[1]) + x[0]
+        return wall_x - pole_x
+
+    # Distance from the tip of the pole to the ground
+    def dist_pole_ground(x):
+        pole_y = pole_length * sym.sin(x[1]) + cart_height
+        return pole_y
+
+    # Distance from the front of the cart to the wall
+    def dist_cart_wall(x):
+        cart_x = x[0] + 0.5 * cart_width
+        return wall_x - cart_x
+
+    return [dist_pole_wall, dist_pole_ground, dist_cart_wall]
+
+
+def test_planner():
     tree = RigidBodyTree()
     AddModelInstanceFromUrdfFile("resources/cartpole2.urdf", 
                                  FloatingBaseType.kFixed,
@@ -32,33 +58,39 @@ def main():
     plant = RigidBodyPlant(tree)
     context = plant.CreateDefaultContext()
 
-    #def running_cost(x, u):
-    #    return 10. * (u[0]**2 + u[1]**2)
-    
     input_weight = 1.
     goal_weight = 30.
     def running_cost(x, u):
         return input_weight * (u[0]**2 + u[1]**2) + goal_weight * (x[0] - 2.)**2
 
-    import pydrake.symbolic as sym
+    signed_dist_funcs = init_signed_dist_funcs()
 
-    def signed_dist_func(x):
-        pole_x = 0.5 * sym.cos(x[1]) + x[0]
-        #pole_x = 0
-        return 2. - pole_x
-
-    planner = Planner(plant, context, running_cost, signed_dist_func)
+    planner = Planner(plant, context, running_cost, signed_dist_funcs)
 
     #initial_state = (0., math.pi, 0., 0.)
     initial_state = (0., 0., 0., 0.)
     #final_state = (1., 3*math.pi/2., 0., 0.)
     final_state = None
-    x_traj, total_cost = planner._solve_traj_opt(initial_state, final_state)
+    duration_bounds = (3., 3.)
+    x_traj, total_cost = planner._solve_traj_opt(initial_state, final_state, duration_bounds)
+
+    # from pydrake.all import PiecewisePolynomial
+    # tmp = PiecewisePolynomial.FirstOrderHold([0., 1.],
+    #                                          np.column_stack((initial_state,
+    #                                                           initial_state)))
+    # x_traj = x_traj + tmp
+
+    duration = x_traj.end_time() - x_traj.start_time()
+    print("Trajectory duration is {} s".format(duration))
 
     vis = PlanarRigidBodyVisualizer(tree, xlim=[-2.5, 2.5], ylim=[-1, 2.5])
     ani = vis.animate(x_traj, repeat=True)
 
-    plt.show()    
+    plt.show()
+
+
+def main():
+    test_planner()
 
 
 if __name__ == '__main__':
